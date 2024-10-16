7f867effc29091b4127c201b095fd3636991a792
saurabhchaunde <chaundes@gmail.com>
2024-10-16 15:21:46 +0530

    Junit Testing with Spring Boot

---
+++ src/test/java/com/Banking/Banking_app/Calculators.java
@@ -0,0 +1,11 @@
+package com.Banking.Banking_app;
+
+public class Calculators {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int product(int a, int b){
+        return a*b;
+    }
+}
